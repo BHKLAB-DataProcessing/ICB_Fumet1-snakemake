f76b272a25fca6509e5c73fd16e046ffa5b3fa2d
Minoru Nakano <minoru.nakano@gmail.com>
2022-07-08 16:26:35 -0400

    update clin processing

--- scripts/Format_CLIN.R
+++ scripts/Format_CLIN.R
@@ -3,15 +3,17 @@ input_dir <- args[1]
 output_dir <- args[2]
 
 source("https://raw.githubusercontent.com/BHKLAB-Pachyderm/ICB_Common/main/code/Get_Response.R")
+source("https://raw.githubusercontent.com/BHKLAB-Pachyderm/ICB_Common/main/code/format_clin_data.R")
 
 ## Format Expression file
 expr = read.csv(file.path(input_dir, "EXPR.csv.gz")  , sep=";", stringsAsFactors=FALSE )
 write.table( expr , file= file.path(output_dir, "EXPR.csv") , quote=FALSE , sep=";" , col.names=TRUE , row.names=TRUE )
 
 ## Format clinical data
-clin = read.csv( file.path(input_dir, "CLIN.txt"), stringsAsFactors=FALSE , sep="\t")
+clin_original = read.csv( file.path(input_dir, "CLIN.txt"), stringsAsFactors=FALSE , sep="\t")
 
-clin = cbind( clin[ , c( "ID" , "Sex" , "AgeDGM" , "Histology" , "Tumor.stage" , "PFS" , "evtPFS" ) ] , "Lung" , "PD-1/PD-L1" , NA , NA, NA , NA , NA, NA , NA )
+selected_cols <- c( "ID" , "Sex" , "AgeDGM" , "Histology" , "Tumor.stage" , "PFS" , "evtPFS" )
+clin = cbind( clin_original[ , selected_cols] , "Lung" , "PD-1/PD-L1" , NA , NA, NA , NA , NA, NA , NA )
 colnames(clin) = c( "patient" , "sex" , "age"  ,"histo"  , "stage" , "t.pfs" , "pfs" , "primary" , "drug_type" , "response.other.info" , "response"  , "t.os" , "os", "recist" , "dna" , "rna")
 
 clin$rna = "tpm"
@@ -24,6 +26,10 @@ clin$stage = ifelse( clin$stage %in% 3 , "III" ,
 
 clin = clin[ , c("patient" , "sex" , "age" , "primary" , "histo" , "stage" , "response.other.info" , "recist" , "response" , "drug_type" , "dna" , "rna" , "t.pfs" , "pfs" , "t.os" , "os" ) ]
 clin$patient = paste( "P" , clin$patient , sep="" )
+
+clin_original$ID = paste( "P" , clin_original$ID , sep="" )
+rownames(clin) <- clin$patient
+clin = format_clin_data(clin_original, "ID", selected_cols, clin)
 
 write.table( clin , file=file.path(output_dir, "CLIN.csv") , quote=FALSE , sep=";" , col.names=TRUE , row.names=FALSE )
 
